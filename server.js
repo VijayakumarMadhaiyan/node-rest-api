41e84a9ee3e3c23415d244bbf3a3d9dde4489e71
saravana-zt123 <saravanakumar.2701@gmail.com>
2019-08-08 16:57:37 +0530

    CORS Support Added

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || '3300';
 
 app.use(bodyParser.json());
 
+/* CORS config */
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, X-Requested-With, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, DELETE, PATCH, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.get('/getmyName', (req, res, next) => {
   res.status(200).json({ route: req.query.name ? req.query.name : 'No Name Specified in the QueryParam' });
 });
